172c199abd6ebb75724f4ed6730756ac33a28115
uuso <mymail.uuso@gmail.com>
2020-03-28 23:00:03 +0300

    added: log to file, default serial, timestamps

---
+++ arduino_temp_serial_dht11/arduino_temp_serial_dht11.ino
@@ -0,0 +1,30 @@
+#include "ArduinoJson.h" //https://arduinojson.org/v6/doc/
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define DELAY_SEC 20
+DHT dht(DHTPIN, DHTTYPE);
+StaticJsonDocument<70> doc;
+
+char tempstr[100];
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start Serial");
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  bool err = (isnan(h) || isnan(t));
+
+  doc["error"] = err;
+  doc["humidity"] = h;
+  doc["temperature"] = t;
+
+  serializeJson(doc, tempstr);
+  Serial.println(tempstr);
+  delay(1000 * DELAY_SEC);
+}
--- receiver.py
+++ receiver.py
@@ -3,9 +3,10 @@
 
 import serial
 from serial.tools import list_ports
-import time
+import time, json, datetime
+# import json
 
-def choose_comport():
+def choose_comport(default = False):
     coms = list_ports.comports(True)
     if not len(coms):
         print("No COM-devices available.\n")
@@ -15,14 +16,46 @@ def choose_comport():
     while not val:
         print("Available COM-devices:")
         for _, com in zip(range(1,len(coms)+1), coms):
-            print("{}: {}".format(_, com))
-        val = int(input("Select COM-device: "))
+            print("{}: {}".format(_, com))        
+
+        val = default if default else int(input("Select COM-device: "))
+
         if val-1 not in range(len(coms)):
             print("Wrong value. Please try again.")
             val = 0
-        print()
-
+            if default: # Если выбранное значение default не из списка портов
+                return val
+    if default:
+        print("Selected COM-device (default): %d" % (default))
+    print()
     return coms[val-1]
     
+def serial_messaging_json(comport, delay = 0.5):
+    ser = serial.Serial(port = comport.device)
+    while True:        
+        if ser.in_waiting:
+            data = {}
+            msg = ser.read_until()[:-2].decode('utf-8') # [:-2] - из-за последних двух байтов '\r\n'            
+            try:
+                data = json.loads(msg)
+                data["timestamp"] = datetime.datetime.now().isoformat()
+                yield data
+            except Exception as ex:
+                print("Error \"{}\" at message: \"{}\"".format(ex, msg))
+        time.sleep(delay)
 
-print(choose_comport())
+
+if __name__ == "__main__":
+    comport = choose_comport(1)
+    if not comport:
+        exit()
+    
+    message = serial_messaging_json(comport)
+    with open("log.txt", "a", encoding="utf-8") as logfile:
+        while True:
+            msg = next(message)
+            logfile.write("{}\n".format(json.dumps(msg)))
+            print(msg)
+            
+        
+        
