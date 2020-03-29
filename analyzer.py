5b4c247210445c13cf86a215d7da1f8ed5b4ffbe
uuso <mymail.uuso@gmail.com>
2020-03-29 14:37:38 +0300

    add: fill analytic container

--- analyzer.py
+++ analyzer.py
@@ -4,24 +4,49 @@ from datetime import datetime, timedelta
 from receiver import log_filepath
 from file_read_backwards import FileReadBackwards
 
-def fill_analytics(minutes = 30):
+class TempAnalytics:
     """
-    Заполнит систему предыдущими значениями (за последние полчаса по-умолчанию)
-    если такие имеются.
+Класс должен внутри себя держать данные за последние <_oldest_minutes> минут,
+при дополнении новыми записями - освобождать самые старые.
+Каждые <_rescan_minutes> перепроверять события:
+- превышен ли допустимый предел отклоненияя температуры <_temp_delta> 
+за последние list(<10> <30> <60> <120> <all>) минут
+- допустимая погрешность измерений датчика будет <_inacc> градуса
+- в записях время записи будет храниться в формате UNIX-time (секунды с начала эпохи)
+для более быстрого сравнения
     """
-    border = datetime.now() - timedelta(minutes=minutes)
-    with FileReadBackwards(log_filepath, encoding="utf-8") as logfile:
-        readed = 0
-        for line in logfile:
-            data = json.loads(line)
-            data_ts = isoparse(data["timestamp"])
-            if data_ts < border:
-                break            
-            print(line)
-            readed += 1
-        print("Analytics filled with {notes} notes of the last {minutes} minutes.".format(
-            notes=readed, minutes = minutes))
+    _oldest_minutes = 20#60*4
+    _rescan_minutes = 5
+    _temp_delta = 2
+    _timers = [10, 30, 60, 120]
+    _inacc = 0.5
+
+    notes = []
+
+    def __init__(self, filepath = log_filepath, minutes = _oldest_minutes):
+        """Заполнит систему предыдущими значениями (за последние 4 часа по-умолчанию)
+        если такие имеются.
+        """
+        border = (datetime.now() - timedelta(minutes=minutes)).timestamp()
+        with FileReadBackwards(log_filepath, encoding="utf-8") as logfile:
+            readed = 0
+            errors = 0
+            for line in logfile:
+                parsed_line = json.loads(line)
+                parsed_line["timestamp"] = isoparse(parsed_line["timestamp"]).timestamp() # ISO8601 преобразуем в UNIX-time
+                if parsed_line["timestamp"] < border:
+                    break
+                if parsed_line['error']: # не учитываем ошибки чтения с датчика
+                    errors += 1
+                else:
+                    self.notes.append(parsed_line)
+                    readed += 1
+            self.notes.reverse()
+            print("Analytics filled with {notes} notes of the last {minutes} minutes.\nErrors: {errors}.".format(
+                notes = readed, minutes = minutes, errors = errors))
+            for note in self.notes:
+                print(note)
 
 
 if __name__ == "__main__":
-    fill_analytics()
+    TA = TempAnalytics()
